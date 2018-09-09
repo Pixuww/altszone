bd3db06a8477391929e56c466872a4ba7d419a69
Pixuww <43114458+Pixuww@users.noreply.github.com>
2018-09-09 18:20:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const token = process.env.token;
 
 var bot = new Discord.Client();
 var prefix = ("!");
@@ -11,7 +12,7 @@ bot.on('ready', () =>{
     console.log("Prêt à être utilisé");
 });
 
-bot.login('NDg2MjcxNjcwMjc4MTYwMzg1.DnW82A.AWrtDf9W9WJXXWfCFxyWEhqCcCc');
+bot.login(token);
 
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "welcome").send(`:tada: Welcome ${member} feel free to visit our website __**www.altszone.pw**__ and read the rules :closed_lock_with_key: `);
